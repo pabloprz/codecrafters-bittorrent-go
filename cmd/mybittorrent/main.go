16c4b403beb25a58677da86247d921b163e69df9
Pablo Perez <pablo.perez.rodriguez@udc.es>
2024-10-07 19:31:32 -0500

    decoding strings

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -16,48 +17,48 @@ var _ = json.Marshal
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
 func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
+	i := 0
 
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
+	for i = 0; i < len(bencodedString); i++ {
+		if !unicode.IsDigit(rune(bencodedString[i])) {
+			break
 		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
 	}
+
+	if i == 0 {
+		return nil, errors.New("was expecting an number, found something else")
+	}
+
+	length, err := strconv.Atoi(bencodedString[:i])
+	if err != nil {
+		return nil, err
+	}
+
+	if bencodedString[i] != ':' {
+		return nil, errors.New("was expecting a ':', found something else")
+	}
+
+	if i+1+length > len(bencodedString) {
+		return nil, errors.New("found end of input, was expecting more characters")
+	}
+
+	return bencodedString[i+1 : i+1+length], nil
 }
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	command := os.Args[1]
 
 	if command == "decode" {
-		// Uncomment this block to pass the first stage
-		//
-		// bencodedValue := os.Args[2]
-		//
-		// decoded, err := decodeBencode(bencodedValue)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// 	return
-		// }
-		//
-		// jsonOutput, _ := json.Marshal(decoded)
-		// fmt.Println(string(jsonOutput))
+		bencodedValue := os.Args[2]
+
+		decoded, err := decodeBencode(bencodedValue)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		jsonOutput, _ := json.Marshal(decoded)
+		fmt.Println(string(jsonOutput))
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
