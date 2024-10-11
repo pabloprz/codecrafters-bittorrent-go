121fade90fab574cfe202b428168e3407b91addb
Pablo Perez <pablo.perez.rodriguez@udc.es>
2024-10-11 13:45:36 -0500

    getting piece length and piece hashes

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha1"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"reflect"
@@ -28,6 +29,44 @@ func parseTorrentFile(filePath string) (map[string]interface{}, error) {
 	return res.(map[string]interface{}), nil
 }
 
+func printTorrentInfo(torrentFilePath string) {
+	fileContents, err := parseTorrentFile(torrentFilePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("Tracker URL: %s\n", fileContents["announce"])
+
+	info, ok := fileContents["info"].(map[string]interface{})
+	if !ok {
+		panic(errors.New("Error extracting info"))
+	}
+
+	fmt.Printf("Length: %d\n", info["length"])
+
+	encodedInfo, err := utils.EncodeBencode(reflect.ValueOf(info))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	hasher := sha1.New()
+	hasher.Write([]byte(encodedInfo))
+
+	fmt.Printf("Info Hash: %x\n", hasher.Sum(nil))
+	fmt.Printf("Piece Length: %d\n", info["piece length"])
+
+	pieces, ok := info["pieces"].(string)
+	if !ok {
+		panic(errors.New("Error extrating pieces"))
+	}
+
+	fmt.Println("Piece Hashes:")
+	for i := 0; i < len(pieces); i += 20 {
+		fmt.Printf("%x\n", pieces[i:i+20])
+	}
+}
+
 func main() {
 	command := os.Args[1]
 
@@ -44,32 +83,7 @@ func main() {
 		fmt.Println(string(jsonOutput))
 	} else if command == "info" {
 		torrentFilePath := os.Args[2]
-
-		fileContents, err := parseTorrentFile(torrentFilePath)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Printf("Tracker URL: %s\n", fileContents["announce"])
-
-		info, ok := fileContents["info"].(map[string]interface{})
-
-		if ok {
-			fmt.Printf("Length: %d\n", info["length"])
-
-			encodedInfo, err := utils.EncodeBencode(reflect.ValueOf(info))
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			hasher := sha1.New()
-			hasher.Write([]byte(encodedInfo))
-			fmt.Printf("Info Hash: %x\n", hasher.Sum(nil))
-		} else {
-			fmt.Println("Error extracting length")
-		}
+		printTorrentInfo(torrentFilePath)
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
