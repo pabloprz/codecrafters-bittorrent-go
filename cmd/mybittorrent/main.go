83d81f799796407817fc0e87eae91fe878f26a2a
Pablo Perez <pablo.perez.rodriguez@udc.es>
2024-10-14 19:34:29 -0500

    getting peers

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"crypto/sha1"
+	"encoding/binary"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"reflect"
+	"strconv"
 
 	"github.com/codecrafters-io/bittorrent-starter-go/internal/utils"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
+)
+
+type TorrentInfo struct {
+	url         string
+	length      int
+	infoHash    string
+	pieceLength int
+	pieces      []string
+	peers       []string
+}
+
+const (
+	N_PEERS           = 3
+	PEER_SIZE         = 6
+	PEER_ADDRESS_SIZE = 4
+	PEER_PORT_SIZE    = 2
 )
 
 // Ensures gofmt doesn't remove the "os" encoding/json import (feel free to remove this!)
@@ -29,41 +48,118 @@ func parseTorrentFile(filePath string) (map[string]interface{}, error) {
 	return res.(map[string]interface{}), nil
 }
 
-func printTorrentInfo(torrentFilePath string) {
+func getTorrentInfo(torrentFilePath string) (*TorrentInfo, error) {
 	fileContents, err := parseTorrentFile(torrentFilePath)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil, err
 	}
-
-	fmt.Printf("Tracker URL: %s\n", fileContents["announce"])
 
 	info, ok := fileContents["info"].(map[string]interface{})
 	if !ok {
 		panic(errors.New("Error extracting info"))
 	}
 
-	fmt.Printf("Length: %d\n", info["length"])
-
 	encodedInfo, err := utils.EncodeBencode(reflect.ValueOf(info))
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil, err
 	}
 	hasher := sha1.New()
 	hasher.Write([]byte(encodedInfo))
-
-	fmt.Printf("Info Hash: %x\n", hasher.Sum(nil))
-	fmt.Printf("Piece Length: %d\n", info["piece length"])
 
 	pieces, ok := info["pieces"].(string)
 	if !ok {
 		panic(errors.New("Error extrating pieces"))
 	}
 
-	fmt.Println("Piece Hashes:")
+	torrentInfo := TorrentInfo{
+		url:         fileContents["announce"].(string),
+		length:      info["length"].(int),
+		infoHash:    string(hasher.Sum(nil)),
+		pieceLength: info["piece length"].(int),
+		pieces:      make([]string, 0, len(pieces)/20),
+	}
+
 	for i := 0; i < len(pieces); i += 20 {
-		fmt.Printf("%x\n", pieces[i:i+20])
+		torrentInfo.pieces = append(torrentInfo.pieces, pieces[i:i+20])
+	}
+
+	return &torrentInfo, nil
+}
+
+func printTorrentInfo(torrentFilePath string) {
+	torrentInfo, err := getTorrentInfo(torrentFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Tracker URL: %s\n", torrentInfo.url)
+	fmt.Printf("Length: %d\n", torrentInfo.length)
+	fmt.Printf("Info Hash: %x\n", torrentInfo.infoHash)
+	fmt.Printf("Piece Length: %d\n", torrentInfo.pieceLength)
+	fmt.Println("Piece Hashes:")
+	for _, piece := range torrentInfo.pieces {
+		fmt.Printf("%x\n", piece)
+	}
+}
+
+func getTorrentPeers(torrentInfo *TorrentInfo) (int, []string) {
+	req, err := http.NewRequest(http.MethodGet, torrentInfo.url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	q := req.URL.Query()
+	q.Add("info_hash", torrentInfo.infoHash)
+	q.Add("peer_id", "string_of_length_200")
+	q.Add("port", "6881")
+	q.Add("uploaded", "0")
+	q.Add("downloaded", "0")
+	q.Add("left", strconv.Itoa(torrentInfo.length))
+	q.Add("compact", "1")
+
+	req.URL.RawQuery = q.Encode()
+
+	res, err := http.Get(req.URL.String())
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		panic(errors.New(fmt.Sprintf("error - invalid code received fetching peers %d\n", res.StatusCode)))
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	responseDict, _, err := utils.DecodeBencode(string(body))
+	decoded, ok := responseDict.(map[string]any)
+	if !ok {
+		panic(errors.New("error parsing response"))
+	}
+
+	interval := decoded["interval"].(int)
+
+	peers := make([]string, 0, N_PEERS)
+	peerString := decoded["peers"].(string)
+	for i := 0; i < len(peerString); i += PEER_SIZE {
+		port := binary.BigEndian.Uint16([]byte(peerString[i+PEER_ADDRESS_SIZE : i+PEER_ADDRESS_SIZE+PEER_PORT_SIZE]))
+		peer := fmt.Sprintf("%d.%d.%d.%d:%d", peerString[i], peerString[i+1], peerString[i+2], peerString[i+3], port)
+		peers = append(peers, peer)
+	}
+
+	return interval, peers
+}
+
+func printPeers(torrentInfo *TorrentInfo) {
+	_, peers := getTorrentPeers(torrentInfo)
+
+	for _, peer := range peers {
+		fmt.Println(peer)
 	}
 }
 
@@ -84,6 +180,12 @@ func main() {
 	} else if command == "info" {
 		torrentFilePath := os.Args[2]
 		printTorrentInfo(torrentFilePath)
+	} else if command == "peers" {
+		torrentInfo, err := getTorrentInfo(os.Args[2])
+		if err != nil {
+			panic(err)
+		}
+		printPeers(torrentInfo)
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
